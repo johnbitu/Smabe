ecf2d21ee09076a548dcd1a93720b6232f70c46c
João Victor Bitu <bitujoaovictor@gmail.com>
2025-01-03 13:35:08 -0300

    first commit

---
+++ src/main/java/com/dev/smartbankextract/SmartbankextractApplication.java
@@ -0,0 +1,13 @@
+package com.dev.smartbankextract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartbankextractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartbankextractApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/smartbankextract/SmartbankextractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.smartbankextract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartbankextractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
