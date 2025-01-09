d16255ac6d544a49f06cb433fb7a57cde0256354
João Victor Bitu <bitujoaovictor@gmail.com>
2025-01-09 15:49:24 -0300

    Feat: adicionando a coluna Banco, adicionando funcionalidade de criar uma nova aba/página e ordenando de forma decrescente a coluna de data

--- src/main/java/com/dev/smartbankextract/SmartbankextractApplication.java
+++ src/main/java/com/dev/smartbankextract/SmartbankextractApplication.java
@@ -3,7 +3,7 @@ package com.dev.smartbankextract;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
-import com.google.api.services.sheets.v4.model.ValueRange;
+import com.google.api.services.sheets.v4.model.*;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.apache.poi.poifs.crypt.Decryptor;
 import org.apache.poi.poifs.crypt.EncryptionInfo;
@@ -109,6 +109,8 @@ public class SmartbankextractApplication {
 				linha.add(""); // Categoria (vazio conforme especificado)
 				linha.add(""); // Observações (vazio conforme especificado)
 
+				linha.add("NuBank"); // Banco
+
 				registros.add(linha);
 			}
 
@@ -166,6 +168,11 @@ public class SmartbankextractApplication {
 							linha.add(valorEntrada); // Entrada
 							linha.add(valorSaida);
 
+							linha.add(""); // Categoria
+							linha.add(""); // Observações
+
+							linha.add("C6 Bank");
+
 							registros.add(linha);
 						}
 					}
@@ -183,35 +190,59 @@ public class SmartbankextractApplication {
 
 	public static void insertInGoogle(String spreadsheetId, List<List<Object>> registros) throws Exception {
 		logger.info("Iniciando envio ao Google Sheets para ID: " + spreadsheetId);
-		String range = "Dados1!A1"; // Define o intervalo inicial
 		Sheets sheetsService = getSheetsService();
+
+		String sheetName = "SmabeDados";
+
+		// Verifica se a aba 'SmabeDados' existe
+		Integer sheetId = null;
+		try {
+			sheetId = getSheetIdByName(sheetsService, spreadsheetId, sheetName);
+			logger.info("Aba '" + sheetName + "' encontrada.");
+		} catch (RuntimeException e) {
+			logger.info("Aba '" + sheetName + "' não encontrada. Criando uma nova.");
+			createSheet(sheetsService, spreadsheetId, sheetName);
+			sheetId = getSheetIdByName(sheetsService, spreadsheetId, sheetName);
+		}
+
+		// Determina a última linha preenchida
+		String rangeToGetLastRow = sheetName + "!A:A"; // Supondo que os dados estão na aba "Dados1" na coluna A
+		ValueRange response = sheetsService.spreadsheets().values()
+				.get(spreadsheetId, rangeToGetLastRow)
+				.execute();
+
+		List<List<Object>> values = response.getValues();
+		int lastRow = values != null ? values.size() : 0; // Conta o número de linhas preenchidas
+
+		logger.info("Última linha preenchida: " + lastRow);
+
+		// Define o intervalo inicial para adicionar os novos dados
+		String range = sheetName + "!A" + (lastRow + 1); // Começa a partir da próxima linha disponível
 
 		// Monta o layout da planilha
 		List<List<Object>> planilhaComLayout = new ArrayList<>();
 
-		// Adiciona o cabeçalho
-		List<Object> cabecalho = Arrays.asList("Data", "Titulo", "Descrição", "Entrada", "Saída", "Categoria", "Observações");
-		planilhaComLayout.add(cabecalho);
+		// Adiciona o cabeçalho apenas se a planilha estiver vazia
+		if (lastRow == 0) {
+			List<Object> cabecalho = Arrays.asList("Data", "Titulo", "Descrição", "Entrada", "Saída", "Categoria", "Observações", "Banco");
+			planilhaComLayout.add(cabecalho);
+		}
 
 		// Adiciona os dados lidos no layout
 		for (List<Object> registro : registros) {
 			List<Object> linhaFormatada = new ArrayList<>();
 
-			// Supondo que os dados originais são organizados como:
-			// Data, Descrição, Entrada, Saída
-			linhaFormatada.add(registro.size() > 0 ? registro.get(0) : ""); // Data
-			linhaFormatada.add(registro.size() > 1 ? registro.get(1) : ""); // Titulo
-			linhaFormatada.add(registro.size() > 2 ? registro.get(2) : ""); // Descrição
-
 			// Organiza os valores de Entrada e Saída
 			String entrada = registro.size() > 3 ? registro.get(3).toString() : "";
 			String saida = registro.size() > 4 ? registro.get(4).toString() : "";
+			linhaFormatada.add(registro.size() > 0 ? registro.get(0) : ""); // Data
+			linhaFormatada.add(registro.size() > 1 ? registro.get(1) : ""); // Titulo
+			linhaFormatada.add(registro.size() > 2 ? registro.get(2) : ""); // Descrição
 			linhaFormatada.add(!entrada.isEmpty() ? entrada : ""); // Entrada
 			linhaFormatada.add(!saida.isEmpty() ? saida : ""); // Saída
-
-			// Adiciona campos de Categoria e Observações como vazios
-			linhaFormatada.add(""); // Categoria
-			linhaFormatada.add(""); // Observações
+			linhaFormatada.add(""); // Categoria (vazio)
+			linhaFormatada.add(""); // Observações (vazio)
+			linhaFormatada.add(registro.size() > 7 ? registro.get(7) : ""); // Banco
 
 			planilhaComLayout.add(linhaFormatada);
 		}
@@ -221,18 +252,80 @@ public class SmartbankextractApplication {
 			ValueRange body = new ValueRange().setValues(planilhaComLayout);
 
 			sheetsService.spreadsheets().values()
-					.update(spreadsheetId, range, body)
+					.append(spreadsheetId, range, body) // Usando 'append' para adicionar linhas
 					.setValueInputOption("RAW")
 					.execute();
 
 			logger.info("Dados enviados com sucesso ao Google Sheets.");
+
+			ordenarDadosPorColuna(sheetsService, spreadsheetId, "Dados1", 0, "DESCENDING");
 		} catch (Exception e) {
 			logger.log(Level.SEVERE, "Erro ao enviar dados ao Google Sheets", e);
 			throw e;
 		}
+
 	}
 
+	public static void createSheet(Sheets sheetsService, String spreadsheetId, String sheetName) throws Exception {
+		try {
+			BatchUpdateSpreadsheetRequest requestBody = new BatchUpdateSpreadsheetRequest()
+					.setRequests(Collections.singletonList(
+							new Request().setAddSheet(new AddSheetRequest()
+									.setProperties(new SheetProperties().setTitle(sheetName))
+							)
+					));
 
+			sheetsService.spreadsheets().batchUpdate(spreadsheetId, requestBody).execute();
+			logger.info("Aba '" + sheetName + "' criada com sucesso.");
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "Erro ao criar aba no Google Sheets", e);
+			throw e;
+		}
+	}
+
+	public static void ordenarDadosPorColuna(Sheets sheetsService, String spreadsheetId, String sheetName, int columnIndex, String sortOrder) throws Exception {
+		try {
+			Integer sheetId = getSheetIdByName(sheetsService, spreadsheetId, sheetName);
+
+			GridRange sortRange = new GridRange()
+					.setSheetId(sheetId)
+					.setStartRowIndex(1)
+					.setStartColumnIndex(0)
+					.setEndColumnIndex(7);
+
+			SortRangeRequest sortRequest = new SortRangeRequest()
+					.setRange(sortRange)
+					.setSortSpecs(Collections.singletonList(
+							new SortSpec()
+									.setDimensionIndex(columnIndex)
+									.setSortOrder(sortOrder)
+					));
+
+			Request request = new Request().setSortRange(sortRequest);
+			BatchUpdateSpreadsheetRequest batchRequest = new BatchUpdateSpreadsheetRequest()
+					.setRequests(Collections.singletonList(request));
+
+			sheetsService.spreadsheets().batchUpdate(spreadsheetId, batchRequest).execute();
+			logger.info("Coluna ordenada com sucesso em ordem " + sortOrder + ".");
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "Erro ao ordenar dados na planilha", e);
+			throw e;
+		}
+	}
+
+	public static Integer getSheetIdByName(Sheets sheetsService, String spreadsheetId, String sheetName) throws Exception {
+		try {
+			Spreadsheet spreadsheet = sheetsService.spreadsheets().get(spreadsheetId).execute();
+			return spreadsheet.getSheets().stream()
+					.filter(sheet -> sheet.getProperties().getTitle().equals(sheetName))
+					.map(sheet -> sheet.getProperties().getSheetId())
+					.findFirst()
+					.orElseThrow(() -> new RuntimeException("Aba com o nome '" + sheetName + "' não encontrada."));
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "Erro ao obter o ID da aba", e);
+			throw e;
+		}
+	}
 
 	public static Sheets getSheetsService() throws Exception {
 		String credentialsPath = dotenv.get("GOOGLE_CREDENTIALS_PATH");
